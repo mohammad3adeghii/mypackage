88f17018374a80a36a86dda09599edc299bc24bc
Mohammad Sadeghi <mscode01.php@gmail.com>
2024-10-16 06:45:35 +0330

    first commit

---
+++ mypackage/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    description='a simple greeting package',
+    author='mohammad sadeghi',
+    author_email='mscode01.php@gmail.com',
+    url='https://github.com/mohammad3adeghii/',
+)
